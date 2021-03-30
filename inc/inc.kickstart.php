55112bc5f1dbf155cabd0b5037282788767a9acb
YoJaum33 <73176312+YoJaum33@users.noreply.github.com>
2021-03-30 14:31:20 +0200

    Add files via upload

---
+++ inc/inc.kickstart.php
@@ -0,0 +1,7 @@
+<?php
+// on a déplace le conf.bdd car au mauvais endroit
+  require("inc/conf.bdd.php"); // contient les constantes DB_SERVER, DB_DATABASE, DB_USERNAME, DB_PASSWORD
+  require("inc/inc.connexion.php");
+  require("inc/inc.head.php");
+
+?>
